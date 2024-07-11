a4db53209b0838f56311acf9d33c1f1fe574637e
ribeiromarialazara1 <ribeiromarialazara1@gmail.com>
2024-07-11 18:16:56 -0300

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 #outputs
-output "instance_public_ip" {
-  description = "O IP público da instância EC2"
-  value       = aws_instance.app-orbbi.public_ip
-}
+#output "instance_public_ip" {
+#  description = "O IP público da instância EC2"
+ # value       = aws_instance.app-orbbi.public_ip
+#}
