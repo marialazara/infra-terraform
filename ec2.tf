4ba1416a3ee4ae3a3ea7956adeafeaa5c9b98290
ribeiromarialazara1 <ribeiromarialazara1@gmail.com>
2024-07-11 18:29:50 -0300

    alteracoes ec2

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app_orbbi" {
   ami                    = "ami-06c68f701d8090592" #Amazon Linux 2023 AMI
   instance_type          = "t2.micro"
-  associate_public_ip_address = yes
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.security_group.id]
   user_data              = file("./user_data.sh")
   key_name = "ssh-orbi-challenge"
