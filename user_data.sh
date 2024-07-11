2e1569f277ac6927844d0efd3be2e87f08e69cf3
ribeiromarialazara1 <ribeiromarialazara1@gmail.com>
2024-07-11 15:54:19 -0300

    alterado o userdata

--- user_data.sh
+++ user_data.sh
@@ -5,4 +5,4 @@ yum update -y
 yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
-docker run -p 80:80 --name app-orbbi marialazararibeiro/orbbi-app:latest
+docker run -p 80:80 --rm --name app-orbbi marialazararibeiro/orbbi-app
