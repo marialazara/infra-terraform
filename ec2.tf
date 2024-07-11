ea492a7586a7eb22f75a954c18def0abd8aafea4
ribeiromarialazara1 <ribeiromarialazara1@gmail.com>
2024-07-11 17:25:55 -0300

    trocando tipo da instância

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 #Instância
 resource "aws_instance" "app-orbbi" {
   ami                    = "ami-06c68f701d8090592" #Amazon Linux 2023 AMI
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   vpc_security_group_ids = [aws_security_group.security_group.id]
   user_data              = file("./user_data.sh")
   key_name = "ssh-orbi-challenge"
