002ec9b73578fb6a7b8e6ba654873f45abd63fc9
ribeiromarialazara1 <ribeiromarialazara1@gmail.com>
2024-07-11 18:23:57 -0300

    fix

--- ec2.tf
+++ ec2.tf
@@ -2,11 +2,17 @@
 resource "aws_instance" "app_orbbi" {
   ami                    = "ami-06c68f701d8090592" #Amazon Linux 2023 AMI
   instance_type          = "t2.micro"
-  associate_public_ip_address = data.aws_eip.eip_orbbi_app.id
+  associate_public_ip_address = yes
   vpc_security_group_ids = [aws_security_group.security_group.id]
   user_data              = file("./user_data.sh")
   key_name = "ssh-orbi-challenge"
   tags = {
     Name = "orbbi-app-instance"
   }
+}
+
+# Associação do Elastic IP
+resource "aws_eip_association" "eip_orbbi_app" {
+  instance_id   = aws_instance.app_orbbi.id
+  allocation_id = data.aws_eip.eip_orbbi_app.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-#outputs
-#output "instance_public_ip" {
-#  description = "O IP público da instância EC2"
- # value       = aws_instance.app-orbbi.public_ip
-#}
+outputs
+output "instance_public_ip" {
+  description = "O IP público da instância EC2"
+  value       = aws_instance.app-orbbi.public_ip
+}
